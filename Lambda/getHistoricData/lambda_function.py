451999f486b2d22096df7e06ab66e3efa9606aeb
Bhagat, Shubham Narendra <snbhagat@iu.edu>
2023-07-17 17:22:29 -0400

    added error handling to the code

--- Lambda/getHistoricData/lambda_function.py
+++ Lambda/getHistoricData/lambda_function.py
@@ -20,7 +20,7 @@ def get_forecast_data_from_s3(model, region, category, max_date):
         df = pd.read_csv(obj['Body'])
         df['date_time'] = pd.to_datetime(df['date_time'])
     else:
-        key = f'{model}/{region}_{category}_forecast.parquet'
+        key = f'{model}/{region}_{category}_forecast1.parquet'
         buffer = io.BytesIO()
         s3.download_fileobj(bucket_name, key, buffer)
         # Reset buffer position to the beginning
@@ -100,153 +100,163 @@ def convert_to_timezone(date, timezone):
     
 def lambda_handler(event, context):
     
-    response = {
-        'statusCode': 200,
-        'headers': {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'OPTIONS,GET',
-            'Access-Control-Allow-Headers': 'Content-Type'
+    try:
+
+        response = {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,GET',
+                'Access-Control-Allow-Headers': 'Content-Type'
+            }
         }
-    }
     
-    # print(event)
-    
-    if event['httpMethod'] == 'OPTIONS':
-        print("options request")
-        return response
-    
-    model = event['queryStringParameters']['model']
-    region = event['queryStringParameters']['region']
-    time = event['queryStringParameters']['time']
-    frequency = event['queryStringParameters']['frequency']
-    
-    print("model:", model)
-    print("region:", region)
-    print("time:", time)
-    print("frequency:", frequency)
-    
-    
-    
-    # Retrieving and processing Historic data from S3
-    historic_df, max_date = get_historic_data_from_s3(region)
-    
-    # Creating two different dataframes for Historic demand and generation data
-    historic_df['date_time'] = historic_df.apply(lambda row: pd.to_datetime(row['Date']), axis=1)
-
-    historic_demand_df = historic_df[['date_time', 'Demand']]
-    historic_demand_df.rename(columns = {'Demand': 'value'}, inplace=True)
-    
-    historic_generation_df = historic_df[['date_time', 'Net generation']]
-    historic_generation_df.rename(columns = {'Net generation': 'value'}, inplace=True)
-    
-    # Aggregate demand and generation data
-    aggregate_historic_demand_df = aggregate_data(historic_demand_df, frequency)
-    aggregate_historic_generation_df = aggregate_data(historic_generation_df, frequency)
-    
-    
-    
-    # # # Convert date_time to epoch time in milliseconds for the trimmed DataFrames
-    # aggregate_historic_demand_df.loc[:, 'epoch_time_ms'] = (aggregate_historic_demand_df['date_time'].astype(int) / 10**6).astype(int)
-    # aggregate_historic_generation_df.loc[:, 'epoch_time_ms'] = (aggregate_historic_generation_df['date_time'].astype(int) / 10**6).astype(int)
-    
-    # Define the time zone
-    target_timezone = 'America/New_York'  # Eastern Time Zone (EDT/EST)
-    
-    # Convert date_time to epoch time in milliseconds with the appropriate time zone
-    aggregate_historic_demand_df['epoch_time_ms'] = aggregate_historic_demand_df['date_time'].apply(
-    lambda x: int(convert_to_timezone(x, target_timezone).timestamp() * 1000)
-)
-
-    aggregate_historic_generation_df['epoch_time_ms'] = aggregate_historic_generation_df['date_time'].apply(
-    lambda x: int(convert_to_timezone(x, target_timezone).timestamp() * 1000)
-)   
-
-    
-
-    # Convert DataFrames to list of lists with epoch time as integers
-    historic_demand_data = aggregate_historic_demand_df[['epoch_time_ms', 'value']].astype({'epoch_time_ms': int}).values.tolist()
-    historic_generation_data = aggregate_historic_generation_df[['epoch_time_ms', 'value']].astype({'epoch_time_ms': int}).values.tolist()
-    
-    
-    
-    # Retrieving and processing Forecast data from S3
-    demand_df = get_forecast_data_from_s3(model, region, "demand", max_date)
-    category = "netgen" if model == "arima" else "generation"
-    generation_df = get_forecast_data_from_s3(model, region, category, max_date)
-    
-  
-    # Aggregate demand and generation data
-    aggregate_demand_df = aggregate_data(demand_df, frequency)
-    aggregate_generation_df = aggregate_data(generation_df, frequency)
-    
-    # Trim demand and generation data
-    trimmed_demand_df = trim_data(aggregate_demand_df, time)
-    trimmed_generation_df = trim_data(aggregate_generation_df, time)
-
-    # # Convert date_time to epoch time in milliseconds for the trimmed DataFrames
-    # trimmed_demand_df.loc[:, 'epoch_time_ms'] = (trimmed_demand_df['date_time'].astype(int) / 10**6).astype(int) 
-    # trimmed_generation_df.loc[:, 'epoch_time_ms'] = (trimmed_generation_df['date_time'].astype(int) / 10**6).astype(int)
-
-     # Convert date_time to epoch time in milliseconds with the desired time zone offset
-    # trimmed_demand_df['epoch_time_ms'] = trimmed_demand_df['date_time'].apply(
-    #     lambda x: int((datetime.fromisoformat(str(x)) + target_offset).timestamp() * 1000)
-    # )
-    
-    # trimmed_generation_df['epoch_time_ms'] = trimmed_generation_df['date_time'].apply(
-    #     lambda x: int((datetime.fromisoformat(str(x)) + target_offset).timestamp() * 1000)
-    # )
-
-
-    # Convert date_time to epoch time in milliseconds with the appropriate time zone
-    trimmed_demand_df['epoch_time_ms'] = trimmed_demand_df['date_time'].apply(
-    lambda x: int(convert_to_timezone(x, target_timezone).timestamp() * 1000)
-)
-
-    trimmed_generation_df['epoch_time_ms'] = trimmed_generation_df['date_time'].apply(
-    lambda x: int(convert_to_timezone(x, target_timezone).timestamp() * 1000)
-) 
-
-    # Convert the trimmed DataFrames to list of lists with epoch time as integers
-    forecast_demand_data = trimmed_demand_df[['epoch_time_ms', 'value']].astype({'epoch_time_ms': int}).values.tolist()
-    forecast_generation_data = trimmed_generation_df[['epoch_time_ms', 'value']].astype({'epoch_time_ms': int}).values.tolist()
-    
-    
-    
-    
-    
-    
-    # result = {
-    #     'forecast_demand_data':forecast_demand_data,
-    #     'forecast_generation_data':forecast_generation_data,
-    #     'historic_demand_data':historic_demand_data,
-    #     'historic_generation_data':historic_generation_data
-    # }
-    # result = {
-    #     # 'forecast_demand_data':forecast_demand_data,
-    #     # 'forecast_generation_data':forecast_generation_data,
-    #     'historic_demand_data':historic_demand_data,
-    #     'historic_generation_data':historic_generation_data
-    # }
-    
-    
-    
-    result = {
-        'model' : model,
-        'region' : region,
-        'time' : time,
-        'frequency' : frequency,
-        'historic_demand_data': historic_demand_data,
-        'historic_generation_data': historic_generation_data,
-        'forecast_demand_data': forecast_demand_data,
-        'forecast_generation_data': forecast_generation_data
+        # print(event)
         
-    }
+        if event['httpMethod'] == 'OPTIONS':
+            print("options request")
+            return response
+        
+        model = event['queryStringParameters']['model']
+        region = event['queryStringParameters']['region']
+        time = event['queryStringParameters']['time']
+        frequency = event['queryStringParameters']['frequency']
+        
+        print("model:", model)
+        print("region:", region)
+        print("time:", time)
+        print("frequency:", frequency)
+        
+        
+        
+        # Retrieving and processing Historic data from S3
+        historic_df, max_date = get_historic_data_from_s3(region)
+        
+        # Creating two different dataframes for Historic demand and generation data
+        historic_df['date_time'] = historic_df.apply(lambda row: pd.to_datetime(row['Date']), axis=1)
+
+        historic_demand_df = historic_df[['date_time', 'Demand']]
+        historic_demand_df.rename(columns = {'Demand': 'value'}, inplace=True)
+        
+        historic_generation_df = historic_df[['date_time', 'Net generation']]
+        historic_generation_df.rename(columns = {'Net generation': 'value'}, inplace=True)
+        
+        # Aggregate demand and generation data
+        aggregate_historic_demand_df = aggregate_data(historic_demand_df, frequency)
+        aggregate_historic_generation_df = aggregate_data(historic_generation_df, frequency)
+        
+        
+        
+        # # # Convert date_time to epoch time in milliseconds for the trimmed DataFrames
+        # aggregate_historic_demand_df.loc[:, 'epoch_time_ms'] = (aggregate_historic_demand_df['date_time'].astype(int) / 10**6).astype(int)
+        # aggregate_historic_generation_df.loc[:, 'epoch_time_ms'] = (aggregate_historic_generation_df['date_time'].astype(int) / 10**6).astype(int)
+        
+        # Define the time zone
+        target_timezone = 'America/New_York'  # Eastern Time Zone (EDT/EST)
+        
+        # Convert date_time to epoch time in milliseconds with the appropriate time zone
+        aggregate_historic_demand_df['epoch_time_ms'] = aggregate_historic_demand_df['date_time'].apply(
+        lambda x: int(convert_to_timezone(x, target_timezone).timestamp() * 1000))
+
+        aggregate_historic_generation_df['epoch_time_ms'] = aggregate_historic_generation_df['date_time'].apply(
+        lambda x: int(convert_to_timezone(x, target_timezone).timestamp() * 1000) )   
+
+        
+
+        # Convert DataFrames to list of lists with epoch time as integers
+        historic_demand_data = aggregate_historic_demand_df[['epoch_time_ms', 'value']].astype({'epoch_time_ms': int}).values.tolist()
+        historic_generation_data = aggregate_historic_generation_df[['epoch_time_ms', 'value']].astype({'epoch_time_ms': int}).values.tolist()
+        
+        
+        
+        # Retrieving and processing Forecast data from S3
+        demand_df = get_forecast_data_from_s3(model, region, "demand", max_date)
+        category = "netgen" if model == "arima" else "generation"
+        generation_df = get_forecast_data_from_s3(model, region, category, max_date)
+        
     
-    return {
-        'statusCode': 200,
-        'headers': {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        },
-        'body': json.dumps(result)
-    }
+        # Aggregate demand and generation data
+        aggregate_demand_df = aggregate_data(demand_df, frequency)
+        aggregate_generation_df = aggregate_data(generation_df, frequency)
+        
+        # Trim demand and generation data
+        trimmed_demand_df = trim_data(aggregate_demand_df, time)
+        trimmed_generation_df = trim_data(aggregate_generation_df, time)
+
+        # # Convert date_time to epoch time in milliseconds for the trimmed DataFrames
+        # trimmed_demand_df.loc[:, 'epoch_time_ms'] = (trimmed_demand_df['date_time'].astype(int) / 10**6).astype(int) 
+        # trimmed_generation_df.loc[:, 'epoch_time_ms'] = (trimmed_generation_df['date_time'].astype(int) / 10**6).astype(int)
+
+        # Convert date_time to epoch time in milliseconds with the desired time zone offset
+        # trimmed_demand_df['epoch_time_ms'] = trimmed_demand_df['date_time'].apply(
+        #     lambda x: int((datetime.fromisoformat(str(x)) + target_offset).timestamp() * 1000)
+        # )
+        
+        # trimmed_generation_df['epoch_time_ms'] = trimmed_generation_df['date_time'].apply(
+        #     lambda x: int((datetime.fromisoformat(str(x)) + target_offset).timestamp() * 1000)
+        # )
+
+
+        # Convert date_time to epoch time in milliseconds with the appropriate time zone
+        trimmed_demand_df['epoch_time_ms'] = trimmed_demand_df['date_time'].apply(
+        lambda x: int(convert_to_timezone(x, target_timezone).timestamp() * 1000) )
+
+        trimmed_generation_df['epoch_time_ms'] = trimmed_generation_df['date_time'].apply(
+        lambda x: int(convert_to_timezone(x, target_timezone).timestamp() * 1000)) 
+
+        # Convert the trimmed DataFrames to list of lists with epoch time as integers
+        forecast_demand_data = trimmed_demand_df[['epoch_time_ms', 'value']].astype({'epoch_time_ms': int}).values.tolist()
+        forecast_generation_data = trimmed_generation_df[['epoch_time_ms', 'value']].astype({'epoch_time_ms': int}).values.tolist()
+        
+        
+        
+        
+        
+        
+        # result = {
+        #     'forecast_demand_data':forecast_demand_data,
+        #     'forecast_generation_data':forecast_generation_data,
+        #     'historic_demand_data':historic_demand_data,
+        #     'historic_generation_data':historic_generation_data
+        # }
+        # result = {
+        #     # 'forecast_demand_data':forecast_demand_data,
+        #     # 'forecast_generation_data':forecast_generation_data,
+        #     'historic_demand_data':historic_demand_data,
+        #     'historic_generation_data':historic_generation_data
+        # }
+        
+        
+        
+        result = {
+            'model' : model,
+            'region' : region,
+            'time' : time,
+            'frequency' : frequency,
+            'historic_demand_data': historic_demand_data,
+            'historic_generation_data': historic_generation_data,
+            'forecast_demand_data': forecast_demand_data,
+            'forecast_generation_data': forecast_generation_data
+            
+        }
+        
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            'body': json.dumps(result)
+        }
+
+    except Exception as e:
+        # If an exception occurs, return an error response with a proper status code and message
+        error_response = {
+            'statusCode': 500,  # Internal Server Error
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            'body': json.dumps({'message': 'There was some error.'})
+        }
+        return error_response
